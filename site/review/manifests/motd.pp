92dda77da74752d2b4b947b77fc8497a821a4f62
hsravidatta <hsravidatta@yahoo.com>
2017-07-11 22:53:04 +0530

    Update motd.pp

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -1,9 +1,10 @@
 class review::motd {
   notify { 'New class motd' : }
-  
+  $local_scope_variable = "local_values"
+   
   file { 'welcome-template-file':
     path    => '/tmp/motd',
-    content => epp('tmp/motd'),
+    content => epp('motd.epp', { admin_variable => 'hsradmin' }),
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
