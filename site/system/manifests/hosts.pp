47b3e53207253f6e6a2193d93a9b98b191a7f152
busytoday <busytoday@gmail.com>
2017-07-11 14:27:55 -0500

    lab 5.1

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,46 @@
 class system::hosts {
   # paste the output of `puppet resource host` in this class
+  host { 'fidelm.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['fidelm'],
+    ip           => '172.17.0.3',
+    target       => '/etc/hosts',
+  }
+  host { 'ip6-allnodes':
+    ensure => 'present',
+    ip     => 'ff02::1',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-allrouters':
+    ensure => 'present',
+    ip     => 'ff02::2',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-localnet':
+    ensure => 'present',
+    ip     => 'fe00::0',
+    target => '/etc/hosts',
+  }
+  host { 'ip6-mcastprefix':
+    ensure => 'present',
+    ip     => 'ff00::0',
+    target => '/etc/hosts',
+  }
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+    target => '/etc/hosts',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+    target       => '/etc/hosts',
+  }
+  
   # and then purge unmanaged resources
+  resources {'host':
+    purge => true,
+  }
+  
 }
