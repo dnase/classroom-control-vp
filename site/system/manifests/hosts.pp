80fbd573795db56cd93c9d0e499c99ba88f78571
Drew Nase <dnasecs@gmail.com>
2017-07-11 15:30:44 -0400

    lab 5.1

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,35 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+  resources { 'host':
+    purge => true,
+  }
+  host { 'drewbilee.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['drewbilee'],
+    ip           => '172.17.0.2',
+  }
+  host { 'ip6-allnodes':
+    ensure => 'present',
+    ip     => 'ff02::1',
+  }
+  host { 'ip6-allrouters':
+    ensure => 'present',
+    ip     => 'ff02::2',
+  }
+  host { 'ip6-localnet':
+    ensure => 'present',
+    ip     => 'fe00::0',
+  }
+  host { 'ip6-mcastprefix':
+    ensure => 'present',
+    ip     => 'ff00::0',
+  }
+  host { 'localhost':
+    ensure => 'present',
+    ip     => '127.0.0.1',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+  }
 }
