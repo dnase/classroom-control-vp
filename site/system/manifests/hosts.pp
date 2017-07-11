bcd46e6e45efa48d0954ea73cce7b10c34d99523
hkanani <harshil.kanani@cat.com>
2017-07-11 14:31:06 -0500

    lab 5.1

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,43 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+  resources {'host':
+    purge => true,
+  }
+  
+  host { 'hkanani.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['hkanani'],
+    ip           => '172.17.0.7',
+  }
+  host { 'ip6-allnodes':
+    ensure => 'present',
+    ip     => 'ff02::1',
+  }
+  host { 'ip6-allrouters':
+    ensure => 'present',
+    ip     => 'ff02::2',
+  }
+  host { 'ip6-localnet':
+    ensure => 'present',
+    ip     => 'fe00::0',
+  }
+  host { 'ip6-mcastprefix':
+    ensure => 'present',
+    ip     => 'ff00::0',
+  }
+  host { 'localhost':
+    ensure => 'present',
+    host_aliases => [
+        'localhost.localdomain',
+        'localhost4',
+        'localhost4.localdomain4',
+        'training.puppetlabs.vm',
+        'training'
+        ],
+    ip     => '127.0.0.1',
+  }
+  host { 'master.puppetlabs.vm':
+    ensure       => 'present',
+    host_aliases => ['puppet'],
+    ip           => '172.17.0.1',
+  }
 }
