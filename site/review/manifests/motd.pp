028f0ade0ee65f5ceabc2e8a6d4f4eeeb288fdf2
hsravidatta <hsravidatta@yahoo.com>
2017-07-11 22:49:19 +0530

    Update motd.pp

--- site/review/manifests/motd.pp
+++ site/review/manifests/motd.pp
@@ -1,3 +1,10 @@
 class review::motd {
   notify { 'New class motd' : }
+  
+  file { 'welcome-template-file':
+    path    => '/tmp/motd',
+    content => epp('tmp/motd'),
+    mode    => '0644',
+    owner   => 'root',
+    group   => 'root',
 }
