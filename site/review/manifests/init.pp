70e3eec1648d00edf3b5ae7c057771ed75d3b168
hemakumar8 <hemakumar8@gmail.com>
2017-07-11 21:20:15 +0530

    Update init.pp

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -1,12 +1,3 @@
-class review {
-  # this class should accept a parameter rather than having
-  # the username hardcoded.
-
-  # Uncomment and use this variable where appropriate
-#  $homedir = $user ? {
-#    'root'  => '/root',
-#    default => "/home/$user",
-#  }
 class review(
   $user='review',
   ) {
