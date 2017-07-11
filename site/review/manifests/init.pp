a2f59c3d2825fb7237b2c0adf7bc52f7d615d357
busytoday <busytoday@gmail.com>
2017-07-11 12:34:25 -0500

    lab 3.3 - included motd

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -1,6 +1,7 @@
 class review ($user = 'review',) {
   # this class should accept a parameter rather than having
   # the username hardcoded.
+  include review::motd
 
   # Uncomment and use this variable where appropriate
   $homedir = $user ? {
