a285b3a02f0b32fd6445874ea99e45653fe8973c
kishanambi <ambi.kishan@gmail.com>
2017-07-11 15:28:44 -0400

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,22 @@
-class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
+class system::hosts{
+resources {'host':
+purge => true,
+}
+host { 'kishanambi.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['kishanambi'],
+  ip           => '172.17.0.11',
+  target       => '/etc/hosts',
+}
+host { 'localhost':
+  ensure => 'present',
+  ip     => '127.0.0.1',
+  target => '/etc/hosts',
+}
+host { 'master.puppetlabs.vm':
+  ensure       => 'present',
+  host_aliases => ['puppet'],
+  ip           => '172.17.0.1',
+  target       => '/etc/hosts',
+}
 }
