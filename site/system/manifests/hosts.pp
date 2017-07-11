51b390f94784dd26314dffd06b4c25c2ea4ec0d2
kishanambi <ambi.kishan@gmail.com>
2017-07-11 15:30:46 -0400

    lab 5.1

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -6,17 +6,14 @@ host { 'kishanambi.puppetlabs.vm':
   ensure       => 'present',
   host_aliases => ['kishanambi'],
   ip           => '172.17.0.11',
-  target       => '/etc/hosts',
 }
 host { 'localhost':
   ensure => 'present',
   ip     => '127.0.0.1',
-  target => '/etc/hosts',
 }
 host { 'master.puppetlabs.vm':
   ensure       => 'present',
   host_aliases => ['puppet'],
   ip           => '172.17.0.1',
-  target       => '/etc/hosts',
 }
 }
