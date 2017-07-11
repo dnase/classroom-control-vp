37235d7ad6305c3257a3af24faa200dd27aaa9f5
Brian Walton <brian@brimus.org>
2017-07-11 12:02:47 -0400

    Update init.pp

--- site/review/manifests/init.pp
+++ site/review/manifests/init.pp
@@ -19,8 +19,8 @@ class review (
 
   file { "${homedir}/.bashrc":
     ensure => file,
-    owner  => '$user',
-    group  => '$user',
+    owner  => "$user",
+    group  => "$user",
     mode   => '0644',
     source => 'puppet:///modules/review/bashrc'
   }
