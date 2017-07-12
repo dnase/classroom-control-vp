a21d27e9d6bbc7868eec1454adb18c151eac951f
busytoday <busytoday@gmail.com>
2017-07-12 14:41:14 -0500

    lab 9.2 - fixed errors

--- site/kerberos/manifests/init.pp
+++ site/kerberos/manifests/init.pp
@@ -2,7 +2,7 @@ class kerberos {
    # use an augeas resource to manage the default_realm
    # determine the proper context with the augtool shell
    augeas {'krb5.conf':
-      context => '/files/etc/krb5.conf/libdefaults/default_realm',
+      context => '/files/etc/krb5.conf/libdefaults',
       changes => 'set default_realm PUPPETLABS.VM',
    }
 
