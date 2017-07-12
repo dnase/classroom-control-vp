488052806c65b771aa8ea722d758c757cddbfbb0
Drew Nase <dnasecs@gmail.com>
2017-07-12 13:10:02 -0400

    lab 7.2 - exported resources

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -1,8 +1,12 @@
 class system::classroom {
   # export a virtual host resource for yourself
-
+  @@host { $::fqdn:
+    ip => $::ipaddress,
+    aliases => [$::hostname],
+    tag => 'classroom',
+  }
 
   # collect all resources from the database (including your own)
   # enforce only those tagged with `classroom`.
-  
+  Host <<| tag == 'classroom' |>>
  }
