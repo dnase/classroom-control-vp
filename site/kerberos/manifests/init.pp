3e76a8385ed7fa85ec7594e09301f3612fc4c99b
Kunal <mahi.kunal@gmail.com>
2017-07-13 00:56:16 +0530

    Update init.pp

--- site/kerberos/manifests/init.pp
+++ site/kerberos/manifests/init.pp
@@ -1,5 +1,9 @@
 class kerberos {
    # use an augeas resource to manage the default_realm
+   augeas { 'krb5.conf':
+     context => '/files/etc/krb5.conf/libdefaults',
+     changes => 'set default_realm PUPPETLABS.VM',
+    }
    # determine the proper context with the augtool shell
 
 }
