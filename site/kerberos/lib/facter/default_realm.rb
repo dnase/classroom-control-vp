23c6377e5cbd050182b6c1344921b174af393451
Drew Nase <dnasecs@gmail.com>
2017-07-12 15:44:13 -0400

    Update default_realm.rb

--- site/kerberos/lib/facter/default_realm.rb
+++ site/kerberos/lib/facter/default_realm.rb
@@ -1,4 +1,7 @@
+require 'augeas'
 # Write a fact that returns the value of running the command
-Facter.add("default_realm") do
-  setcode "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
+Facter.add(:default_realm) do
+  Augeas::open do |aug|
+    setcode aug.get('/files/etc/krb5.conf/libdefaults/default_realm')
+  end
 end
