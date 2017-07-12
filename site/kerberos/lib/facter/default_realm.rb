055b865711392c22b0260619651fd310e003292f
joshb89 <joshbonneville@icloud.com>
2017-07-12 12:55:29 -0500

    code cleanup

--- site/kerberos/lib/facter/default_realm.rb
+++ site/kerberos/lib/facter/default_realm.rb
@@ -1,4 +1,4 @@
 # Write a fact that returns the value of running the command
-Facter.add("defaut_realm") do
-setcode "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
+Facter.add("default_realm") do
+  setcode "/bin/awk '/default_realm/{print $NF}' /etc/krb5.conf"
 end
