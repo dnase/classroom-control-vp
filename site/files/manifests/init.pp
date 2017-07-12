bc2473f70c00fe49e6f793d5b82e138cb400876c
Drew Nase <dnasecs@gmail.com>
2017-07-12 15:14:23 -0400

    lab 9.1 - file manipulation

--- site/files/manifests/init.pp
+++ site/files/manifests/init.pp
@@ -7,22 +7,40 @@ class files {
   file { '/etc/cron.allow':
     ensure => file,
   }
+  file { '/etc/cron.deny':
+    ensure => file,
+  }
   file_line { 'allow root cron jobs':
     ensure => present,
     path   => '/etc/cron.allow',
     line   => 'root',
   }
   # Add a rule to cron.deny to deny jobs by default
-
-
+  file_line { 'deny root cron jobs':
+    ensure => present,
+    path => '/etc/cron.deny',
+    line => '*',
+  }
+  
+  concat { '/etc/motd':
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
   # What concat resource is needed for this fragment to work?
   concat::fragment { 'motd header':
     target  => '/etc/motd',
     order   => '01',
     content => epp('files/motd_header.epp'),
   }
-
-  # Add a few fragments to be appended to /etc/motd
-
-
+  concat::fragment { 'motd #1':
+    target => '/etc/motd',
+    order => '02',
+    content => 'This is moar stuff',
+  }
+  concat::fragment { 'motd #2':
+    target => '/etc/motd',
+    order => '03',
+    content => 'This is the last of the stuff',
+  }
 }
