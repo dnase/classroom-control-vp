29f1b402efe30fd061a3b40479c73882a8c87c1b
jyan98 <jyan98@yahoo.com>
2017-07-12 13:19:08 -0400

    Lab 7.2

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,4 +1,35 @@
 class system::hosts {
   # paste the output of `puppet resource host` in this class
   # and then purge unmanaged resources
+  class system::hosts {
+  Host {
+   ensure => present,
+ }
+  resources { 'host':
+  purge => true,
+}
+  host { 'ip6-allnodes':
+    ip     => 'ff02::1',
+  }
+  host { 'ip6-allrouters':
+    ip     => 'ff02::2',
+  }
+  host { 'ip6-localnet':
+  ip     => 'fe00::0',
+}
+
+  host { 'ip6-mcastprefix':
+    ip     => 'ff00::0',
+  }
+  host { 'localhost':
+    ip     => '127.0.0.1',
+  }
+host { 'master.puppetlabs.vm':
+
+    host_aliases => ['puppet'],
+
+    ip           => '172.17.0.1',
+
+  }
+}
 }
