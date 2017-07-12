d5f202b323d073ff2fd29d0ec8692ffae5ba0413
hkanani <harshil.kanani@cat.com>
2017-07-12 09:23:24 -0500

    lab 6.2

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,32 +1,23 @@
 class system::admins {
   require mysql::server
-  
-  $retired = [ 'ralph' ]
-  $admins = {
-    'brad' => { max_queries_per_hour => '600' },
-    'monica' => { max_queries_per_hour => '600' },
-    'luke' => { max_queries_per_hour => '600' },
-    'zack' => { max_queries_per_hour => '1200' },
-  }
-
-  $admins.each |$user, $params| {
-    mysql_user { "${user}@localhost":
-      ensure => present,
-      max_queries_per_hour => $params['max_queries_per_hour'],
-    }
-
-    user { $user:
-      ensure => present,
-      managehome => true,
-    }
-  }
-
-  $retired.each |$user| {
-    mysql_user { "${user}@localhost":
+  $users = {
+    'zack' => {
+      max_queries_per_hour => 1200,
+    },
+    'monica' => {},
+    'brad' => {},
+    'luke' => {},
+    'ralph' => {
       ensure => absent,
+    },
+  }
+  $users.each |$username, $params| {
+    mysql_user { "${username}@localhost":
+      ensure => pick($params['ensure'], present),
+      max_queries_per_hour => pick($params['max_queries_per_hour'], 600),
     }
-
-    user { $user:
-      ensure => absent,
+    user { $username:
+      ensure => pick($params['ensure'], present),
     }
+  }
 }
