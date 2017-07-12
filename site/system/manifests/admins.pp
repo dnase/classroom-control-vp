0121349f249c78050fe303d0a8a9daae0dbfd424
hsravidatta <hsravidatta@yahoo.com>
2017-07-12 19:56:36 +0530

    Update admins.pp

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -1,23 +1,29 @@
 class system::admins {
   require mysql::server
   
-  $mysql_users_present = { 'zack' => '1200', 'monica' => '600', 'brad' => '600', 'luke' => '600' }
-  $mysql_users_absent = [ 'ralph' ]
+  $users = {
+     'zack' => { 
+       max_queries_per_hour => 1200,
+      },
+      
+      'monica' => {},
+      'brad' => {},
+      'luke' => {},
+      'ralph' => {
+        ensure => absent,
+      },
+  }
   
-  $mysql_users_present.each |String $mysql_each_user, String $max_queries_hour| {
-     mysql_user { "${mysql_each_user}@localhost" :
+  $users.each |$username, $params|
+     mysql_user { "${username}@localhost" :
+       ensure => pick($params['ensure'], present),
+       max_queries_per_hour => pick($params['max_queries_per_hour'], 600)
+     }
+     
+     user { $username:
        ensure => present,
-       max_queries_per_hour => $max_queries_hour,
      }
   }
   
-  $mysql_users_absent.each |String $mysql_each_user| {
-     mysql_user { "${mysql_each_user}@localhost" :
-       ensure => absent,
-    }
-  }
- 
-  user { ['zack', 'monica', 'ralph', 'brad', 'luke']:
-    ensure => present,
-  }
-}
+ } 
+  
