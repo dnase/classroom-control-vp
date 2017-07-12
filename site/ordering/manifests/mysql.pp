a13d4affef6be75e2b335858d5a4eac6425c5a0e
Kunal <mahi.kunal@gmail.com>
2017-07-12 20:58:40 +0530

    Update mysql.pp

--- site/ordering/manifests/mysql.pp
+++ site/ordering/manifests/mysql.pp
@@ -1,13 +1,5 @@
 class ordering::mysql {
-  # How do we make sure these classes don't float off the relationship graph?
-  class { '::mysql::server':
-    root_password    => 'strongpassword',
-  }
-
-  class { '::mysql::bindings':
-    php_enable  => true,
-    perl_enable => true,
-  }
+  # How do we make sure these classes don't float off the relationship graph
   contain ::mysql::bindings
   contain ::mysql::server
 }
