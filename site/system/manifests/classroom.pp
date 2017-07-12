9e96388a83f79e34b505831865be4eed9c13e62a
Brian Walton <brian@brimus.org>
2017-07-12 13:03:51 -0400

    Update classroom.pp

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -2,7 +2,7 @@ class system::classroom {
   # export a virtual host resource for yourself
   @@host{ $::fqdn:
     ensure => 'present',
-    host_alias => [$::hostname],
+    host_aliases => [$::hostname],
     ip => $::ipaddress,
     tag => 'classroom',
   }
