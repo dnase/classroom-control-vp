d0a47ac29d7001a32fe660830bd1a439f318256f
Kunal <mahi.kunal@gmail.com>
2017-07-12 18:16:47 +0530

    Update hosts.pp

--- site/system/manifests/hosts.pp
+++ site/system/manifests/hosts.pp
@@ -1,39 +1,31 @@
 class system::hosts {
-  # paste the output of `puppet resource host` in this class
-  # and then purge unmanaged resources
-  
+  Host {
+    ensure => present,
+  }
   resources { 'host':
     purge => true,
   }
-  host { 'kunal.puppetlabs.vm':
-    ensure       => 'present',
+  host { 'drewbilee.puppetlabs.vm':
     host_aliases => ['kunal'],
     ip           => '172.17.0.6',
   }
   host { 'ip6-allnodes':
-    ensure => 'present',
     ip     => 'ff02::1',
   }
   host { 'ip6-allrouters':
-    ensure => 'present',
     ip     => 'ff02::2',
   }
   host { 'ip6-localnet':
-    ensure => 'present',
     ip     => 'fe00::0',
   }
   host { 'ip6-mcastprefix':
-    ensure => 'present',
     ip     => 'ff00::0',
   }
   host { 'localhost':
-    ensure => 'present',
     ip     => '127.0.0.1',
   }
   host { 'master.puppetlabs.vm':
-    ensure       => 'present',
     host_aliases => ['puppet'],
     ip           => '172.17.0.1',
   }
-
 }
