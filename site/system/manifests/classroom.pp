2c3353a600389bf3a4c05dcb8ebadeb86ee9a304
Drew Nase <dnasecs@gmail.com>
2017-07-12 13:13:04 -0400

    Update classroom.pp

--- site/system/manifests/classroom.pp
+++ site/system/manifests/classroom.pp
@@ -1,4 +1,5 @@
 class system::classroom {
+  include system::hosts
   # export a virtual host resource for yourself
   @@host { $::fqdn:
     ip => $::ipaddress,
@@ -9,8 +10,4 @@ class system::classroom {
   # collect all resources from the database (including your own)
   # enforce only those tagged with `classroom`.
   Host <<| tag == 'classroom' |>>
-  
-  resources { 'host':
-    purge => true,
-  }
  }
