f9c1601cc39eacee5ceee1e581fdb91e198f0dd8
busytoday <busytoday@gmail.com>
2017-07-12 09:31:25 -0500

    lab 6.2 - fixed more syntax errors

--- site/system/manifests/admins.pp
+++ site/system/manifests/admins.pp
@@ -2,10 +2,10 @@ class system::admins {
   require mysql::server
   
   $admin_list = {
-    zack = {max_queries_per_hour => 1200},
-    monica = {max_queries_per_hour => 600},
-    brad = {max_queries_per_hour => 600},
-    luke = {max_queries_per_hour => 600},
+    'zack' => {max_queries_per_hour => 1200},
+    'monica' => {max_queries_per_hour => 600},
+    'brad' => {max_queries_per_hour => 600},
+    'luke' => {max_queries_per_hour => 600},
   }
   
   $admin_list.each |$admin,$param| {
@@ -19,7 +19,7 @@ class system::admins {
   }
     
   $retired_list = {
-    ralph = {ensure = absent},
+    'ralph' => {ensure = absent},
   }
   
   $retired_list.each |$admin,$param| {
