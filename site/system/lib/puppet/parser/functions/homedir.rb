dba8cf44c71a913832bcc798a21085365804b9bb
Drew Nase <dnasecs@gmail.com>
2017-07-12 14:17:25 -0400

    lab 8.2 - custom functions

--- site/system/lib/puppet/parser/functions/homedir.rb
+++ site/system/lib/puppet/parser/functions/homedir.rb
@@ -1,2 +1,11 @@
 # Write a simple function called homedir that accepts a single string argument.
 # It should return the conventional Linux home directory based on a username
+Puppet::Parser::Functions.newfunction(:homedir, :type => :rvalue, :arity => 1) do |args|
+  user = args[0]
+  case user
+  when 'root'
+    return '/root'
+  else
+    return "/home/#{user}"
+  end
+end
