592b6e90194bf3c703465c3faaeb522fb7ddb13d
jdalexand <joshua.d.alexander@outlook.com>
2017-07-12 14:17:48 -0500

    Update init.pp

--- site/files/manifests/init.pp
+++ site/files/manifests/init.pp
@@ -20,10 +20,10 @@ class files {
 }
 
   # What concat resource is needed for this fragment to work?
-  concat::fragment { 'motd header':
-    target  => '/etc/motd',
-    order   => '01',
-    content => epp('files/motd_header.epp'),
+  concat { '/etc/motd':
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
   }
 
   # Add a few fragments to be appended to /etc/motd
@@ -40,12 +40,12 @@ class files {
   concat::fragment { 'motd #1':
     target => '/etc/motd',
     order => '02',
-    content => 'This is moar stuff',
+    content => "This is moar stuff\n",
   }
   concat::fragment { 'motd #2':
     target => '/etc/motd',
     order => '03',
-    content => 'This is the last of the stuff',
+    content => "This is the last of the stuff\n",
   }
 
 }
